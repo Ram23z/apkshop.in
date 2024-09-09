9d5462e402747fbd1222a34abd6b03d30768b3af
Ram23z <125743551+Ram23z@users.noreply.github.com>
2024-09-09 07:03:14 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,11 @@ export default function Footer() {
       <>
       <footer className="footer footer-center bg-base-100 border-t-2 text-base-content rounded p-10">
   <nav className="grid grid-flow-col gap-4">
-    <a  href="/about"  className="link link-hover">About</a>
-    <a href="/contact"  className="link link-hover">Contact</a>
-    <a href="/privacy-policy"  className="link link-hover">Privacy</a>
-    <a href="/terms" className="link link-hover">Terms</a>
+    <a  href="/about"  className="link link-hover">About Us</a>
+    <a href="/contact"  className="link link-hover">Contact Us</a>
+    <a href="/privacy-policy"  className="link link-hover">Privacy Policy</a>
+        <a href="/cookies"  className="link link-hover">Cookies Privacy</a>
+    <a href="/terms" className="link link-hover">Terms and conditions</a>
   </nav>
   
   <aside>
